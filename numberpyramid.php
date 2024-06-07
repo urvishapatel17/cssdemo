2eef11ec325c4b147707871d1b041924338f4fba
Urvisha <urvi17npatel@gmail.com>
2024-06-07 18:42:21 +0530

    Test code : number pyramid

---
+++ numberpyramid.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title></title>
+</head>
+<body>
+	<?php
+		$s = 5;
+		for ($i=$s; $i >= 1 ; $i--) { 
+			for ($j=$i; $j >= 1 ; $j--) { 
+				echo $j." ";
+			}
+			echo "<br>";
+		}
+	?>
+	<p>Hello</p>
+</body>
+</html>
